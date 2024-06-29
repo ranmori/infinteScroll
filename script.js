6958419aba888b7c1e5c45d8f145a01f36979ad7
Naema <94893024+ranmori@users.noreply.github.com>
2024-06-29 12:55:12 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,82 @@
 const imageContainer = document.getElementById('image-container');
-
-let photoArray=[];
-let loadedImage=0;
-let totalImages;
-let count=0;
-const YOUR_ACCESS_KEY = "zr0gSKKmFZQ0G406HDw7yKpmHZEBkzFdHKuwBQWNJ_M";
+// make an image array
+let photoArray = [];
+let loadedImage = 0;
+let totalImages = 0;
+// you can also set count to 0
+let count = 30;
 let ready = false;
-let apiUrl=`https://api.unsplash.com/photos/random?client_id=${YOUR_ACCESS_KEY}&count=${count}`
+
+// get the api from unsplash free api
+const apiKey = 'MSa4BcQDK0Re27ekRQl5ibIXSzTMsaLXHSYN5sg8J44';
+const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
 
 
 
 function imageLoaded(){
 console.log("ready")
- loadedImages++;
-if (totalImages===loadedImages){
-     count=10;
+ loadedImage++;
+if (loadedImage === totalImages){
       ready=true;
   console.log('loaded');
 }
 }
+function setAttributes(element,attributes){
+  for (const key in attributes){
+    element.setAttribute(key,attributes[key]);
+  }
+}
+
+// add to dom
+
+function displayPhoto(){
+  loadedImage=0;
+
+  totalImages = photoArray.length;
+  photoArray.forEach((photo)=>{
+    //create a link to unsplash
+    const item=document.createElement('a');
+    setAttributes(item,
+                  { href: photo.links.html,
+      target: '_blank',
+                       });
+    // create an image element
+    const img = document.createElement('img');
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+    // check if image is loaded
+    img.addEventListener('load', imageLoaded);
+    //add image to imageconatiner and link element
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+}
+// get the photos using async and await
 async  function getPhotos(){
   try {
     const response=  await fetch(apiUrl);
       photoArray  = await response.json();
-    console.log("photoArray");
+    //console.log("photoArray");
     displayPhoto();
   }
   catch(error) {
-    console.log("error");
+    //console.log("error");
   }
 }
-// create elements for links & photos, add to DOM
- photoArray.forEach((photo)=>{
-     
-     const item = document.createElement('a');
-     //item.setAttribute('href',photo.links.html);
-     //item.setAttribute('target','_blank');
-     setAttributes(item,{href:photo.links.html, target:'_blank'});
-     const image = document.createElement('img');
-     //image.setAttribute('src',photo.urls.regular);
-     //image.setAttribute('alt',photo.current_user_collections.description);
-     //image.setAttribute('title',photo.current_user_collections.description);
-     setAttributes(image,
-        {
-            src: photo.urls.regular,
-            alt: photo.alt_description
-        });
-    image.addEventListener('load',imageLoaded);
-     
-     item.appendChild(image);
-     imageContainer.appendChild(item);
- });
-}
+// check if scroll on the bottom
 
-
-window.addEventListener('scroll',()=>{
+window.addEventListener('scroll', () => {
   
-  if (window.innerHeight+ window.scrollY >= document.body.offsetHeight - 1000 && ready){
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready){
+     ready=false;
     getPhotos();
-    ready=false;
-    console.log("scrolled")
+   
+    console.log("scrolled");
   }
-})
+});
+
+// on load call function
+getPhotos();
